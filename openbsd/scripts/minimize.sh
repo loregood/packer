8e5f549549befa0dca7cd223fde3b29450ee8a65
Nicolai Brunvoll Ernst <nicolai.ernst@deic.dk>
2019-10-22 13:12:30 +0200

    changed minimize to look in /etc/fstab

--- openbsd/scripts/minimize.sh
+++ openbsd/scripts/minimize.sh
@@ -1,44 +1,17 @@
 #!/bin/sh
 #
-# Fill all partions with zeros for a smaller box size.
+# Fill all active partions with zeros for a smaller box size.
 
 set -e
 
-## /
-/bin/dd if=/dev/zero of=/EMPTY bs=1M || true
-/bin/rm -f /EMPTY
+## Find all the active partitions, except swap (none)
+partitions=$(/usr/bin/awk '{print $2}' /etc/fstab |grep -v none)
 
-## /home
-/bin/dd if=/dev/zero of=/home/EMPTY bs=1M || true
-/bin/rm -f /home/EMPTY
+## For each partition zero out the remaining space with an empty file
+for partition in ${partitions}; do
+  /bin/dd if=/dev/zero of=${partition}/EMPTY bs=1M || true
+  /bin/rm -f ${partition}/EMPTY
+done
 
-## /tmp
-/bin/dd if=/dev/zero of=/tmp/EMPTY bs=1M || true
-/bin/rm -f /tmp/EMPTY
-
-## /usr
-/bin/dd if=/dev/zero of=/usr/EMPTY bs=1M || true
-/bin/rm -f /usr/EMPTY
-
-## /usr/X11R6
-/bin/dd if=/dev/zero of=/usr/X11R6/EMPTY bs=1M || true
-/bin/rm -f /usr/X11R6/EMPTY
-
-## /usr/local
-/bin/dd if=/dev/zero of=/usr/local/EMPTY bs=1M || true
-/bin/rm -f /usr/local/EMPTY
-
-## /usr/obj
-/bin/dd if=/dev/zero of=/usr/obj/EMPTY bs=1M || true
-/bin/rm -f /usr/obj/EMPTY
-
-## /usr/src
-/bin/dd if=/dev/zero of=/usr/src/EMPTY bs=1M || true
-/bin/rm -f /usr/src/EMPTY
-
-## /var
-/bin/dd if=/dev/zero of=/var/EMPTY bs=1M || true
-/bin/rm -f /var/EMPTY
-
-## Make sure changes are written to disk (flush cache)
+## Make sure changes are written to disk (flush cache).
 /bin/sync
